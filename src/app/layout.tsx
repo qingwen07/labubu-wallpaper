552cb926697923cf56a8ef1d829570b83a0856b0
xqw <xiongqingwen@mycs.cn>
2025-06-19 15:56:36 +0800

    add google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -59,6 +59,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-HWEDR9SKSB"></script>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-HWEDR9SKSB');
+          `
+        }} />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
